ec8aafd861526fd17bb1aeefe296008df0ad4d2d
timcso <timea.jakabfy@spicyanalytics.com>
2016-09-26 21:17:39 +0200

    Add logic16

--- app.js
+++ app.js
@@ -14,9 +14,7 @@
     ToBuy.removeItem = function (index) {
       ShoppingListCheckOffService.removeItem(index);
     }
-
   }
-  
   
   AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
@@ -26,7 +24,6 @@
 
   }
   
-  
   function ShoppingListCheckOffService() {
     var service = this;
   
@@ -35,19 +32,10 @@
     // List of already bought items
     var bought = [];
 
-    to_buy = [{name: 'chips', quantity: '3 bags'}, {name: 'chips', quantity: '3 bags'}, 
-      {name: 'chips', quantity: '3 bags'}, {name: 'chips', quantity: '3 bags'}, 
-      {name: 'chips', quantity: '3 bags'}, {name: 'chips', quantity: '3 bags'}, 
-      {name: 'chips', quantity: '3 bags'}];
- 
-    // service.addItem = function (itemName, quantity) {
-    //   var item = {
-    //     name: itemName,
-    //     quantity: quantity
-    //   };
-
-    //   bought.push(item);
-    // };
+    to_buy = [{name: 'milk', quantity: '1 bottle'}, {name: 'chips', quantity: '3 bags'}, 
+      {name: 'chocolate', quantity: '3 bar'}, {name: 'bread', quantity: '1 bags'}, 
+      {name: 'juice', quantity: '1 bottle'}, {name: 'yogurt', quantity: '3 cup'}, 
+      {name: 'eggs', quantity: '1 dozen'}];
   
     service.removeItem = function (index) {
       var item = {
@@ -57,9 +45,7 @@
       bought.push(item);
       to_buy.splice(index, 1);
     };
-  
-    
-  
+
     service.getToBuy = function () {
       return to_buy;
     };
